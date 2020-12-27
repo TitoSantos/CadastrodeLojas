65379dc9ec6594361047203fa305597768ebf9a1
TitoSantos <tiagom.soft@yahoo.com.br>
2020-12-26 21:04:05 -0300

    Criação dos métodos dentro da classe Loja.

--- Entities/Loja.cs
+++ Entities/Loja.cs
@@ -9,8 +9,8 @@ namespace CadastrodeLojas.Entities
 {
     class Loja
     {
-        public string Nome { get; set; }
         public int Codigo { get; set; }
+        public string Nome { get; set; }
         public TipoLoja TipoLoja { get; set; }
         public double Area { get; set; }
         public List<Loja> Lojas { get; private set; } = new List<Loja>();
@@ -19,18 +19,64 @@ namespace CadastrodeLojas.Entities
         {
 
         }
-
-        public Loja(string nome, int codigo, TipoLoja tipo, double area)
+        public Loja(int codigo)
+        {
+            Codigo = codigo;
+        }
+        public Loja(string nome)
         {
             Nome = nome;
+        }
+        public Loja(TipoLoja tipoloja)
+        {
+            TipoLoja = tipoloja;
+        }
+        public Loja(int codigo, string nome, TipoLoja tipoloja, double area)
+        {
             Codigo = codigo;
-            TipoLoja = tipo;
+            Nome = nome;
+            TipoLoja = tipoloja;
             Area = area;
         }
 
         public void AdicionarLoja(Loja novaloja)
         {
             Lojas.Add(novaloja);
+        }
+
+        public int ConsultaCodigo(int codigo)
+        {
+            int cod = codigo;
+            foreach (Loja loja in Lojas)
+            {
+
+            }
+
+            return cod;
+        }
+
+        public double ValorAluguel()
+        {
+           int tipoLoja = (int)TipoLoja;
+           double valor = 0;
+
+           switch (tipoLoja)
+           {
+                case 0:
+                    valor = Area * 50.00;  
+                    break;
+                case 1:
+                    valor = Area * 65.00;
+                    break;
+                case 2:
+                    valor = Area * 80.00;
+                    break;
+                case 3:
+                    valor = Area * 95.00;
+                    break;
+           }
+           
+           return valor;   
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace CadastrodeLojas
 {
     class Program
     {
-        //static List<Loja> lojas = new List<Loja>();
+        
         static void Main(string[] args)
         {
             MenuPrincipal();
@@ -21,25 +21,18 @@ namespace CadastrodeLojas
         {
             Console.Clear();
             Console.WriteLine("*********************** MENU PRINCIPAL ***********************");
-            Console.WriteLine("1 - Cadastro");
-            Console.WriteLine("2 - Consulta");
-            Console.WriteLine("3 - Relatório");
-            Console.WriteLine("4 - Sair");
+            Console.WriteLine();
+            Console.WriteLine("                        1 - Cadastro");
+            Console.WriteLine("                        2 - Consulta");
+            Console.WriteLine("                        3 - Relatório");
+            Console.WriteLine("                        4 - Sair");
 
-            Console.Write("\nOP.: ");
+            Console.Write("                            \nOP.: ");
 
             int op = 0;
 
-            try
-            {
-                op = int.Parse(Console.ReadLine());
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Opção inválida. Favor informar uma das opções da lista!");
-                Console.ReadKey();
-                MenuPrincipal();
-            }
+            op = int.Parse(Console.ReadLine());
+           
 
             switch (op)
             {
@@ -55,11 +48,12 @@ namespace CadastrodeLojas
                 case 4:
                     return;
                 default:
-                    MenuPrincipal();
+                    Console.WriteLine("Opção inválida. Favor informar uma das opções da lista!");
+                    Console.ReadKey();
                     break;
             }
 
-            //MenuPrincipal();
+            MenuPrincipal();
 
         }  
         
@@ -68,23 +62,69 @@ namespace CadastrodeLojas
 
             Console.Clear();
             Console.WriteLine(" *********************** CADASTRO DE LOJA *********************** ");
-            Console.Write("");
+            Console.WriteLine();
             Console.Write("Informe o código da nova loja: ");
             int codigo = int.Parse(Console.ReadLine());
             Console.Write("Informe o nome da nova loja: ");
             string nome = Console.ReadLine();
-            Console.Write("Informe a área em M²: ");
-            double area = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.Write("Tipo da loja será? (0)Alimentação, (1)Confecção, (2)Livros ou (3)Serviços: ");
             TipoLoja tipo = (TipoLoja)Enum.Parse(typeof(TipoLoja), Console.ReadLine());
+            Console.Write("Informe a área em M²: ");
+            double area = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Loja novaLoja = new Loja(nome, codigo, tipo, area);
+            Loja novaLoja = new Loja(codigo, nome, tipo, area);
             novaLoja.AdicionarLoja(novaLoja);
-            
+
+            Console.WriteLine();
+            Console.Write("Deseja adicionar outra loja (S/N)? ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S')
+            {
+                Cadastro();
+            }
+            else
+            {
+                MenuPrincipal();
+            }
         }
 
         static void Consulta()
         {
+            Console.Clear();
+            Console.WriteLine(" *********************** CONSULTAR DE LOJAS CADASTRADAS *********************** ");
+            Console.WriteLine();
+            Console.WriteLine("                                1 - Por código");
+            Console.WriteLine("                                2 - Por nome");
+            Console.WriteLine("                                3 - Por tipo");
+            Console.WriteLine("                                4 - Por área");
+
+            Console.Write("                                    \nOp.: ");
+
+            int op = 0;
+            op = int.Parse(Console.ReadLine());
+
+            switch (op)
+            {
+                case 1:
+                    ConsultaCodigo();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Favor informar uma das opções da lista!");
+                    Console.ReadKey();
+                    break;
+            }
+
+        }
+
+        static void ConsultaCodigo()
+        {
+            Console.Clear();
+            Console.WriteLine(" *********************** CONSULTA POR CÓDIGO *********************** ");
+            Console.WriteLine();
+            Console.Write("Digite o código da loja: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Loja loja = new Loja(codigo); 
 
         }
         static void Relatorio()
